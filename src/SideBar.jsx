6c719b48800646ce205f7b9acce5d7dd83015634
codebaselinux <basecodeacc@gmail.com>
2023-05-06 23:17:38 +0530

    Create sidebar component

---
+++ src/SideBar.jsx
@@ -0,0 +1,12 @@
+const SideBar = () => {
+    return(
+        <div>
+            <i>A</i>
+            <i>B</i>
+            <i>C</i>
+            <i>D</i>     
+            <i>E</i>   
+        </div>
+    )
+};
+export default SideBar;
