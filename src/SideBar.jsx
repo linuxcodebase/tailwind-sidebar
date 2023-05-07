f19a124da6bb1875a0413963f29cb6488e2e048d
codebaselinux <basecodeacc@gmail.com>
2023-05-07 13:02:08 +0530

    Add tooltips

--- src/SideBar.jsx
+++ src/SideBar.jsx
@@ -1,20 +1,25 @@
 import { BsPlus, BsFillLightningFill, BsGearFill } from 'react-icons/bs';
-import {  FaLaptop, FaAtom} from 'react-icons/fa';
+import { FaLaptop, FaAtom, FaWind } from 'react-icons/fa';
 const SideBar = () => {
     return (
         <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow-lg">
 
-            <SideBarIcon icon={<BsPlus size="32" />} />
-            <SideBarIcon icon={<BsFillLightningFill size="20" />} />
-            <SideBarIcon icon={<FaLaptop size="20" />} />
-            <SideBarIcon icon={<BsGearFill size="22" />} />
-            <SideBarIcon icon={<FaAtom size="22" />} />
+            <SideBarIcon icon={<BsPlus size="32" />} text='Add an item' />
+            <SideBarIcon icon={<BsFillLightningFill size="20" />} text='Lightning bolt' />
+            <SideBarIcon icon={<FaLaptop size="20" />} text='Customize your device' />
+            <SideBarIcon icon={<BsGearFill size="22" />} text='Settings' />
+            <SideBarIcon icon={<FaWind size="22" />} text='Built with Tailwind!' />
+            <SideBarIcon icon={<FaAtom size="22" />} text='Built with React!' />
         </div>
     )
 };
-const SideBarIcon = ({ icon }) => (
-    <div className='sidebar-icon'>
+const SideBarIcon = ({ icon, text = 'tooltip 💡' }) => (
+    <div className="sidebar-icon group">
         {icon}
+        <span class="sidebar-tooltip group-hover:scale-100">
+            {text}
+        </span>
     </div>
-)
-export default SideBar;
+);
+
+export default SideBar
