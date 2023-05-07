6a174d2002366f5cf12c0061d69e87862467857b
codebaselinux <basecodeacc@gmail.com>
2023-05-07 13:21:04 +0530

    Add onClick to icons

--- src/SideBar.jsx
+++ src/SideBar.jsx
@@ -1,24 +1,24 @@
 import { BsPlus, BsFillLightningFill, BsGearFill } from 'react-icons/bs';
-import { FaLaptop, FaAtom, FaReact } from 'react-icons/fa';
+import { FaLaptop, FaReact } from 'react-icons/fa';
 const SideBar = () => {
     return (
         <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow-lg">
 
-            <SideBarIcon icon={<BsPlus size="32" />} text='Add an item' />
-            <SideBarIcon icon={<BsFillLightningFill size="20" />} text='Lightning bolt' />
-            <SideBarIcon icon={<FaLaptop size="20" />} text='Customize your device' />
-            <SideBarIcon icon={<BsGearFill size="22" />} text='Settings' />
-            <SideBarIcon icon={<FaReact size="22" />} text='Built with React!' />
+            <SideBarIcon icon={<BsPlus size="32" />} text='Add an item' onClick={() => console.log('Add clicked')}/>
+            <SideBarIcon icon={<BsFillLightningFill size="20" />} text='Lightning bolt' onClick={() => console.log('Lightning clicked')}/>
+            <SideBarIcon icon={<FaLaptop size="20" />} text='Customize your device' onClick={() => console.log('Laptop clicked')}/>
+            <SideBarIcon icon={<BsGearFill size="22" />} text='Settings' onClick={() => console.log('Settings clicked')}/>
+            <SideBarIcon icon={<FaReact size="22" />} text='Built with React!' onClick={() => console.log('Logo clicked')}/>
         </div>
     )
 };
-const SideBarIcon = ({ icon, text = 'tooltip 💡' }) => (
-    <div className="sidebar-icon group">
+const SideBarIcon = ({ icon, text = 'tooltip 💡' , onClick}) => (
+    <button className="sidebar-icon group" onClick={onClick}>
         {icon}
-        <span class="sidebar-tooltip group-hover:scale-100">
+        <span className="sidebar-tooltip group-hover:scale-100">
             {text}
         </span>
-    </div>
+    </button>
 );
 
 export default SideBar
