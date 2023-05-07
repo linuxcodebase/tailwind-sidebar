85693f4e0ba2d2635857e32b44434fdf22f8f4c6
codebaselinux <basecodeacc@gmail.com>
2023-05-07 12:54:42 +0530

    Minor changes

--- src/SideBar.jsx
+++ src/SideBar.jsx
@@ -1,5 +1,5 @@
-import { BsPlus, BsFillLightningFill, BsGearFill, BsAirplaneEngines } from 'react-icons/bs';
-import { FaFire, FaLaptop, FaAtom} from 'react-icons/fa';
+import { BsPlus, BsFillLightningFill, BsGearFill } from 'react-icons/bs';
+import {  FaLaptop, FaAtom} from 'react-icons/fa';
 const SideBar = () => {
     return (
         <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow-lg">
