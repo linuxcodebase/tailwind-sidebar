4d49a9753acd374ffeb041d60b08284ee172de7d
codebaselinux <basecodeacc@gmail.com>
2023-05-07 12:43:31 +0530

    Create sidebar icons

--- src/SideBar.jsx
+++ src/SideBar.jsx
@@ -1,12 +1,22 @@
+import { BsPlus, BsFillLightningFill, BsGearFill, BsAirplaneEngines } from 'react-icons/bs';
+import { FaFire, FaLaptop, FaAtom} from 'react-icons/fa';
 const SideBar = () => {
-    return(
+    return (
         <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow-lg">
-            <i>A</i>
-            <i>B</i>
-            <i>C</i>
-            <i>D</i>     
-            <i>E</i>   
+
+            <SideBarIcon icon={<FaFire size="28" />} />
+            <SideBarIcon icon={<BsPlus size="32" />} />
+            <SideBarIcon icon={<BsFillLightningFill size="20" />} />
+            <SideBarIcon icon={<FaLaptop size="20" />} />
+            <SideBarIcon icon={<BsGearFill size="22" />} />
+            <SideBarIcon icon={<BsAirplaneEngines size="22" />} />
+            <SideBarIcon icon={<FaAtom size="22" />} />
         </div>
     )
 };
+const SideBarIcon = ({ icon }) => (
+    <div>
+        {icon}
+    </div>
+)
 export default SideBar;
